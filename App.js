987847878fc7971d395d485c24c5de7cea46a984
Roy <royrubin@protonmail.com>
2023-08-10 14:28:43 +0200

    Managing list data

--- App.js
+++ App.js
@@ -4,13 +4,17 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   };
 
   function addGoalHandler() {
-    console.log(enteredGoalText);
+    setCourseGoals(currentCoursGoals => [
+      ...currentCoursGoals, 
+      enteredGoalText,
+    ]);
   };
 
 
@@ -23,7 +27,7 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text> )}
       </View>
     </View>
   );
