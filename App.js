653e777cec33a16c57282bcda48634e46214f822
Roy <royrubin@protonmail.com>
2023-08-11 12:59:24 +0200

    Splitting up components

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 //import { StatusBar } from 'expo-status-bar';
-import { stateState, useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View, ScrollView, FlatList } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, TextInput, View, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -30,9 +31,7 @@ export default function App() {
         <FlatList
           data={courseGoals}
           renderItem={itemData => {
-            return <View style={styles.goalItem}>
-              <Text style={styles.goalText}>{itemData.item.text}</Text>
-            </View>;
+            return <GoalItem/>;
           }} 
           keyExtractor={(item, index) => {
             return item.id;
@@ -68,13 +67,4 @@ const styles = StyleSheet.create({
   goalsContainer: {
     flex: 5
   },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white'
-  }
 });
