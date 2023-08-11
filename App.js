caad9c034c7ed8d828ab15c7d2979ec1a8bdbbb4
Roy <royrubin@protonmail.com>
2023-08-11 13:48:20 +0200

    Deleting items

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
-  
+
   const [courseGoals, setCourseGoals] = useState([]);
 
   function addGoalHandler(enteredGoalText) {
@@ -15,20 +15,34 @@ export default function App() {
     ]);
   };
 
+  function deleteGoalHandler(id) {
+    print(id)
+    setCourseGoals(currentCoursGoals => {
+      return currentCoursGoals.filter((goal) => goal.id !== id);
+    });
+
+  }
+
 
   return (
     <View style={styles.appContainer}>
-      <GoalInput onAddGoal={addGoalHandler}/>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList
           data={courseGoals}
           renderItem={itemData => {
-            return <GoalItem text={itemData.item.text}/>;
-          }} 
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
           keyExtractor={(item, index) => {
             return item.id;
           }}
-          />
+        />
       </View>
     </View>
   );
